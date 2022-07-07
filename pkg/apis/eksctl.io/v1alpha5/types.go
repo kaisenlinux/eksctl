c8051387bfe8858a7e1a331d55cb3d8902374b93
Kevin Chevreuil - Kaisen <kaisen@kaisenlinux.org>
2022-07-07 20:12:38 +0200

    Import 0.104.0 version. Add patch to remove the default dev tag.

--- pkg/apis/eksctl.io/v1alpha5/types.go
+++ pkg/apis/eksctl.io/v1alpha5/types.go
@@ -214,6 +214,9 @@ const (
 	// ClusterNameTag defines the tag of the cluster name
 	ClusterNameTag = "alpha.eksctl.io/cluster-name"
 
+	// ClusterOIDCEnabledTag determines whether OIDC is enabled or not.
+	ClusterOIDCEnabledTag = "alpha.eksctl.io/cluster-oidc-enabled"
+
 	// OldClusterNameTag defines the tag of the cluster name
 	OldClusterNameTag = "eksctl.cluster.k8s.io/v1alpha1/cluster-name"
 
@@ -338,10 +341,10 @@ const (
 
 // Values for core addons
 const (
+	minimumVPCCNIVersionForIPv6 = "1.10.0"
 	VPCCNIAddon                 = "vpc-cni"
 	KubeProxyAddon              = "kube-proxy"
 	CoreDNSAddon                = "coredns"
-	minimumVPCCNIVersionForIPv6 = "1.10.0"
 )
 
 // supported version of Karpenter
